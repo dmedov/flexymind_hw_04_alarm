58d4ab2e83536c36739b7017236a12b8fb5f599a
Den Medov <dengithub@gmail.com>
2013-04-14 17:06:01 +0400

    added time calculate method for alarm

--- src/com/example/flexyalarm/SetAlarmActivity.java
+++ src/com/example/flexyalarm/SetAlarmActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
@@ -14,12 +15,13 @@ public class SetAlarmActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
-    //MediaPlayer mediaPlayer;
+    TimePicker timePicker;
     SimpleAlarm alarm;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setalarm);
+        timePicker = (TimePicker)findViewById(R.id.timePicker);
 
         AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
         alarm = SimpleAlarm.getInstance();
@@ -35,5 +37,29 @@ public class SetAlarmActivity extends Activity {
     public void quitBtnOnClick(View v) {
         finish();
     }
+
+    public void setAlarm(View v) {
+        long time = getNextAlarmTime();
+        alarm.set(time);
+    }
+
+    public long getNextAlarmTime() {
+        Calendar calNow = Calendar.getInstance();
+        Calendar calNext = Calendar.getInstance();
+        int pickerHour = timePicker.getCurrentHour();
+        int pickerMin = timePicker.getCurrentMinute();
+
+        calNext.set(Calendar.HOUR, pickerHour);
+        calNext.set(Calendar.MINUTE, pickerMin);
+
+        // check if alarm time set to next day
+        if (calNext.getTimeInMillis() < calNow.getTimeInMillis()) {
+            calNext.add(Calendar.DATE, 1);
+        }
+
+        return calNext.getTimeInMillis();
+    }
+
+
 }
 
