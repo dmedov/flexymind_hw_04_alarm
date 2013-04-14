272d3f9aa3d9e029d38bb68b83bb565bbe868b49
Den Medov <dengithub@gmail.com>
2013-04-14 17:54:35 +0400

    added toogleBtn handler

--- src/com/example/flexyalarm/SetAlarmActivity.java
+++ src/com/example/flexyalarm/SetAlarmActivity.java
@@ -2,10 +2,8 @@ package com.example.flexyalarm;
 
 import android.app.Activity;
 import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TimePicker;
 import android.widget.ToggleButton;
@@ -23,32 +21,36 @@ public class SetAlarmActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setalarm);
+
         timePicker = (TimePicker)findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(true);
         toogleButton = (ToggleButton)findViewById(R.id.toggleButton);
 
         AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
         alarm = SimpleAlarm.getInstance();
         alarm.init(SetAlarmActivity.this, alarmManager);
 
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.add(Calendar.SECOND, 5);
+        //Calendar calendar = Calendar.getInstance();
+        //calendar.setTimeInMillis(System.currentTimeMillis());
+        //calendar.add(Calendar.SECOND, 5);
 
-        alarm.set(calendar.getTimeInMillis());
+        //alarm.set(calendar.getTimeInMillis());
     }
 
     public void quitBtnOnClick(View v) {
         finish();
     }
 
-    public void toogleBtnHandler(View v) {
+    public void toggleBtnHandler(View v) {
+        Log.e("BOOOOOOOOOM", "bom");
         if (toogleButton.isChecked()) {
+            Log.e("BOOOM", "ok");
             long time = getNextAlarmTime();
             alarm.set(time);
         } else {
+            Log.e("BOOOM", "off");
             alarm.off();
         }
-
     }
 
     public long getNextAlarmTime() {
@@ -60,9 +62,13 @@ public class SetAlarmActivity extends Activity {
         calNext.set(Calendar.HOUR, pickerHour);
         calNext.set(Calendar.MINUTE, pickerMin);
 
-        // check if alarm time set to next day
+        Log.e("h:", String.valueOf(pickerHour));
+        Log.e("m:", String.valueOf(pickerMin));
+
+        // check if alarm time must be set to next day
         if (calNext.getTimeInMillis() < calNow.getTimeInMillis()) {
             calNext.add(Calendar.DATE, 1);
+            Log.e("hour", "added");
         }
 
         return calNext.getTimeInMillis();
